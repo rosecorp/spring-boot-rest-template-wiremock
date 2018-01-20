e584ba481095abf62893267b598fef7efa634dee
jk <dakoma@gmail.com>
2018-01-20 17:34:53 +0000

    JK - no path found exception handling.

---
+++ src/main/java/com/rosecorp/wiremock/controller/ExceptionHandlerController.java
@@ -0,0 +1,20 @@
+package com.rosecorp.wiremock.controller;
+
+import com.rosecorp.wiremock.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResponse requestHandlingNoHandlerFound() {
+        return new ErrorResponse("custom_404", "message for 404 error code");
+    }
+}
